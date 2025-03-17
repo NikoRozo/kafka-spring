dc3e8328f7839e332430cc7cc41fd9d0f49333fe
Nicolás E Rozo E <niko.rozo92@outlook.com>
2025-03-17 18:47:49 -0500

    Add Kafka producer configuration and message sending functionality

--- src/main/java/com/devs4j/kafka/KafkaSpringApplication.java
+++ src/main/java/com/devs4j/kafka/KafkaSpringApplication.java
@@ -1,13 +1,47 @@
 package com.devs4j.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 
 @SpringBootApplication
-public class KafkaSpringApplication {
+public class KafkaSpringApplication implements CommandLineRunner {
+    private final Logger log = LoggerFactory.getLogger(KafkaSpringApplication.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @KafkaListener(topics = "devs4j-topic", groupId = "devs4j-group")
+    public void listen(String message){
+        log.info("Message received {} ", message);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaSpringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        kafkaTemplate.send("devs4j-topic", "Sample message").get(100, TimeUnit.MILLISECONDS);
+        /*
+        // Entrega del mensaje asincrona
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("devs4j-topic", "Sample message");
+        future.whenComplete((result, e) -> {
+            if (e == null){
+                log.info("Message sent successfully: {}", result.getRecordMetadata().offset());
+            } else {
+                log.error("Error sending message", e);
+            }
+        });*/
+    }
 }
--- src/main/java/com/devs4j/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/devs4j/kafka/config/KafkaConfiguration.java
@@ -1,18 +1,34 @@
 package com.devs4j.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
+    public Map<String, Object> producerProperties(){
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
     public Map<String, Object> consumerProperties(){
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -23,6 +39,12 @@ public class KafkaConfiguration {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        DefaultKafkaProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(producerProperties());
+        return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
